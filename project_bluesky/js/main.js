e4c592c9cfccc1186af388e39b32b11c916cf683
Owen Orsetti <owenorsetti@Owens-MacBook-Pro.local>
2018-07-15 16:46:28 +0900

    Adding read page for consideration

---
+++ project_bluesky/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.items-slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+});
